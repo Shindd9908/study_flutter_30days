6836852700880ebb50e43cc19926fcaa96cb824b
DucKhanggg <duckhang0702@gmail.com>
2021-11-04 00:12:31 +0700

    Days 2

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  final int days = 30;
+  final String name = "Shindd";
+
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter by " + name),
+          ),
+        ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:study_flutter_30days/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Welcome to 30 days of Flutter"),
-          ),
-        ),
+        child: HomePage()
       ),
     );
   }
