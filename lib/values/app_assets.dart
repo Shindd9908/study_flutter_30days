9ce78d657ec7d1531ed7f71c65376fcb3a19b58a
khangld@weupgroup.vn <duckhang0702@gmail.com>
2021-12-27 15:23:32 +0700

    day6

--- lib/values/app_assets.dart
+++ lib/values/app_assets.dart
@@ -2,4 +2,5 @@ class AppAssets {
   static const imagePath = "assets/images/";
 
   static const imageLogin = imagePath + "images_login.jpg";
+  static const imageHey = imagePath + "images_hey.jpg";
 }
